f36bde37af5f0cf40f31013adb920630882f3017
raluvy95 <43247023+raluvy95@users.noreply.github.com>
2018-12-23 21:06:43 +0200

    Create reload.js

---
+++ commands/reload.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = (bot, message, args) => {
+
+    if (message.author.id !== "YOUR_USER_ID") return message.channel.send("⛔ **ACCESS DENIED**");
+
+    try {
+        delete require.cache[require.resolve(`./${args[0]}.js`)];
+    } catch (e) {
+
+        return message.channel.send(`Unable to reload: ${args[0]}.js`);
+    }
+
+    message.channel.send(`**Successfully reloaded:** ${args[0]}.js`);
+
+
+}
+
+module.exports.help = {
+  name:("reload")
+}
