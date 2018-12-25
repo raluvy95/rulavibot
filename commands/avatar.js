d9451aa4bd45cfad8946893a8da6ee454b797688
raluvy95 <43247023+raluvy95@users.noreply.github.com>
2018-12-25 11:13:47 +0200

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+
+module.exports.run = (client, message, args, tools) => {
+    
+    let user = message.mentions.users.first() || message.author;
+    
+    const embed = new Discord.MessageEmbed()
+        .setColor(0xffffff)
+        .setTitle(user.username)
+        .setImage(user.avatarURL({size: 2048}))
+        
+    message.channel.send(embed)
+    
+}
+
+module.exports.help = {
+  name:("avatar")
+}
