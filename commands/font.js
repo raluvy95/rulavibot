54e51327665464bfa2465d60fe903dd32337317f
raluvy95 <43247023+raluvy95@users.noreply.github.com>
2018-12-25 09:54:59 +0200

    Update font.js

--- commands/font.js
+++ commands/font.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
  
     let {body} = await superagent
     const text = args.join("+");
-    .get(`https://api.genr8rs.com/Generator/Gaming/Fun/LeetSpeakGenerator?genr8rsUserId=1545723572.85885c21deb4d1ae4&_sText=` + text + `&_sCharacterSet=fancy`)
+    .get("https://api.genr8rs.com/Generator/Gaming/Fun/LeetSpeakGenerator?genr8rsUserId=1545723572.85885c21deb4d1ae4&_sText=" + text + "&_sCharacterSet=fancy")
     if(!{body}) return message.channel.send("OOF?! Try again.")
         message.channel.send({body._sResult})
 
