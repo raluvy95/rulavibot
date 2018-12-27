979d6b6cb1cd01b09957bdb784a46b59f828d2f8
raluvy95 <43247023+raluvy95@users.noreply.github.com>
2018-12-27 14:31:08 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     if(!kUser) return message.channel.send("Can't find user!\n**Use:** `r?ban <user> <reason>`");
     let kReason = args.join(" ").slice(22);
     if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("OOf? **You don't have premission to run this command!**");
-    if(kUser.hasPermission("ADMINISTRATOR")) return message.channel.send(":x: | **That user is an administrator premission.. so I can't ban that!**");
+    if(kUser.hasPermission("ADMINISTRATOR")) return message.channel.send(":x: | **The user you are trying to ban is an Server Administrator, which I can't ban!**");
 
     message.guild.member(kUser).ban(kReason);
     message.channel.send(":ok_hand: | **I banned that guy**");
