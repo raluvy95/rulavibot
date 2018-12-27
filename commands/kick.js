37560dd3439815a73e13b6ddb16516422b6dbed8
raluvy95 <43247023+raluvy95@users.noreply.github.com>
2018-12-27 14:31:33 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     if(!kUser) return message.channel.send("Can't find user!\n**Use:** `r?kick <user> <reason>`");
     let kReason = args.join(" ").slice(22);
     if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("OOf? **You don't have premission to run this command!**");
-    if(kUser.hasPermission("ADMINISTRATOR")) return message.channel.send("**That user is an administrator premission.. so I can't ban that!**");
+    if(kUser.hasPermission("ADMINISTRATOR")) return message.channel.send("**The user you are trying to ban is an Server Administrator, which I can't ban!**");
 
     message.guild.member(kUser).kick(kReason);
     message.channel.send(":ok_hand: | **I kicked that guy!**");
