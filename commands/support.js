61229108e341c70b9fd910822a3d39035928cd88
raluvy95 <43247023+raluvy95@users.noreply.github.com>
2018-12-29 21:36:35 +0200

    Create support.js

---
+++ commands/support.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    message.reply("https://discord.gg/bYYyDVU");
+}
+
+module.exports.help = {
+  name:("support")
+}
