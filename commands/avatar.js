4fd470a531c9a799d75f7062dec9be810ca6ea53
raluvy95 <43247023+raluvy95@users.noreply.github.com>
2018-12-30 19:55:46 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,12 +4,7 @@ module.exports.run = (bot, message, args, tools) => {
     
     let user = message.mentions.users.first() || message.author;
     
-    const embed = new Discord.MessageEmbed()
-        .setColor("#006400")
-        .setTitle(user.username)
-        .setImage(user.avatarURL)
-        
-    message.channel.send(embed)
+    message.channel.send(user.avatarURL)
     
 }
 
